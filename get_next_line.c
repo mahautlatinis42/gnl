784313252a99c7d0d09ed4cfe4b58d879baae8b3
Mahaut LATINIS <mahautlatinis@outlook.com>
2021-02-25 15:48:58 +0100

    fin premiere journee

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,60 @@
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 09:22:55 by malatini          #+#    #+#             */
-/*   Updated: 2021/02/25 10:33:02 by malatini         ###   ########.fr       */
+/*   Updated: 2021/02/25 15:48:28 by malatini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line)
+/*
+int		find_next_end(char *str)
 {
-	int 	i;
-	//void	*buf;
+	int i;
 
 	i = 0;
-	if (fd > 0 && !line && !(*line))
-		return (0);
-	while (i < 3)
-	{	
-		printf("%s\n", line[i]);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			break;
 		i++;
 	}
-	//read(fd, buf, BUFFER_SIZE);
+	return (i);
+}
+*/
+
+int		get_next_line(int fd, char **line)
+{
+	//retourner une ligne lue depuis un file descriptor sans le retour a la ligne
+	static int		count;
+	char			*str;
+	int				read_ret;
+
+	(void)fd;
+	(void)line;
+	read_ret = 0;
+	str = NULL;
+	if (count == 0)
+	{
+		if (!(str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+			return (0);
+	}
+	else
+		read_ret = read(fd, str, BUFFER_SIZE);
+	printf("%d\n", read_ret);
 	return (fd);
 }
 
 int main(void)
 {
-	//Pour ouvrir un fichier on va declarer un pointeur vers un flux 
-	fopen("./texte.txt", r);
-	//get_next_line(1, str);
-	fclose();
+	int fd = open("texte.txt", O_RDONLY);
+	int i = 0;
+	char *line = NULL;
+	while (i < 4)
+	{
+		get_next_line(fd, &line);
+		i++;
+	}
+	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 09:18:44 by malatini          #+#    #+#             */
-/*   Updated: 2021/02/25 10:30:47 by malatini         ###   ########.fr       */
+/*   Updated: 2021/02/25 13:43:54 by malatini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 # define GET_NEXT_LINE__H
 # include <unistd.h>
 # include <libc.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h> 
+# include <fcntl.h>
+
+/*
+typedef struct	s_end_line
+{
+	void	*buf;
+	size_t	nbyte;
+}				t_end_line;
+*/
 
 int		get_next_line(int fd, char **line);
 size_t	ft_strlen(char *str);
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_strjoin(char *s1, char *s2);
+void	ft_bzero(void *s, size_t n);
 
 #endif
