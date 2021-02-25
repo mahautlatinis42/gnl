ae0450953505bba35c6e15507e507bcb700dd0cc
Mahaut Latinis <malatini@student.42.fr>
2021-02-25 17:31:32 +0100

    test guacamole

--- get_next_line.c
+++ get_next_line.c
@@ -6,60 +6,44 @@
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 09:22:55 by malatini          #+#    #+#             */
-/*   Updated: 2021/02/25 15:48:28 by malatini         ###   ########.fr       */
+/*   Updated: 2021/02/25 17:28:38 by malatini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-/*
-int		find_next_end(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			break;
-		i++;
-	}
-	return (i);
-}
-*/
+#define BUFFER_SIZE 100
 
 int		get_next_line(int fd, char **line)
 {
 	//retourner une ligne lue depuis un file descriptor sans le retour a la ligne
-	static int		count;
-	char			*str;
+	//static int		count;
+	char			str[BUFFER_SIZE + 1];
 	int				read_ret;
-
 	(void)fd;
 	(void)line;
-	read_ret = 0;
-	str = NULL;
-	if (count == 0)
-	{
-		if (!(str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+	/*
+	if (!(str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 			return (0);
+	*/
+	read_ret = read(fd, str, BUFFER_SIZE);
+	str[read_ret] = '\0';
+	printf("%s\n", str);
+	/*
+	while ((read_ret = read(fd, str, BUFFER_SIZE) != 0))
+	{
+		str[read_ret] = '\0';
+		//printf("%d\n", read_ret);
+		printf("%s\n", str);
 	}
-	else
-		read_ret = read(fd, str, BUFFER_SIZE);
-	printf("%d\n", read_ret);
+	*/
 	return (fd);
 }
 
 int main(void)
 {
 	int fd = open("texte.txt", O_RDONLY);
-	int i = 0;
 	char *line = NULL;
-	while (i < 4)
-	{
-		get_next_line(fd, &line);
-		i++;
-	}
+	get_next_line(fd, &line);
 	close(fd);
 	return (0);
 }
