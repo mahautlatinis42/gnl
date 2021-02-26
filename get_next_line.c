5b50b0f28f02d757a5c1a1ad34592676b6df0423
Mahaut LATINIS <mahautlatinis@outlook.com>
2021-02-26 15:48:26 +0100

    fin deuxieme journee

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,67 @@
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 09:22:55 by malatini          #+#    #+#             */
-/*   Updated: 2021/02/26 14:12:49 by malatini         ###   ########.fr       */
+/*   Updated: 2021/02/26 15:48:09 by malatini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 100
 
-int		line_is_ok(char *str)
+int		found_n(char *str)
 {
-	int length;
+	int i;
 
-	length = ft_strlen(str) - 1;
-	if (str[length] == '\n')
-		return (1);
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
 	return (0);
 }
 
+void	erase_after_n(char *str)
+{
+	//Je ne modifie pas la string
+	int length;
+	int i;
 
+	if (!found_n(str))
+		return ;
+	i = found_n(str);
+	length = ft_strlen(str);
+	while (length-- > i)
+		str[length] = '\0';
+}
+
+//Maintenant je veux que ma save s arrete au \n sinon je veux passer a la ligne suivante.
+//Je dois boucler en lisant sur le file descriptor pour arriver a la fin de ma liste.
 int		get_next_line(int fd, char **line)
 {
 	char		*buffer;
 	char static *save;
 	int			read_ret;
-	//char		*tmp;
 
 	(void)line;
 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
 	if (!(buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 			return (-1);
-	read_ret = read(fd, buffer, BUFFER_SIZE);
-	buffer[read_ret] = '\0';
-	save = ft_strjoin(save, buffer);
+	while (!found_n(save))
+	{
+		read_ret = read(fd, buffer, BUFFER_SIZE);
+		buffer[read_ret] = '\0';
+		save = ft_strjoin(save, buffer);	
+	}
+	printf("Pas propre : %s\n", save);
+	erase_after_n(save);
 	free(buffer);
-	printf("%s\n", save);
+	printf("Propre : %s\n", save);
+	//Comment je retiens pour la suivante du coup ?
 	return (read_ret);
 }
 
@@ -50,6 +76,7 @@ int main(void)
 	char *line = NULL;
 	get_next_line(fd, &line);
 	/*
+	Je reste bloquee sur une seul ligne
 	get_next_line(fd, &line);
 	get_next_line(fd, &line);
 	get_next_line(fd, &line);
